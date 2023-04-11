5ea716870b0e9492d7d57a984e9e62bfab3efe2c
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-04-11 19:16:05 +0100

    Footer component done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className='footer-container'>
+        <h2>Join our mission. Volunteer, Donate, Advocate. Get Started Today.</h2>
+        <div className='footer-cta'>
+          <div className='footer-contact-info'>
+            <p>
+              Email: <a href='mailto:info@backtoschool.ngo'> info@backtoschool.ngo</a>
+            </p>
+            <p>
+              Call: <a href='tel:2349012345678'> +234 901 234 5678</a>
+            </p>
+          </div>
+
+          <div className='footer-social-wrapper'>
+            <a href='#' target='_blank'>Insta</a>
+            <a href='#' target='_blank'>FB</a>
+            <a href='#' target='_blank'>Twitter</a>
+          </div>
+
+          <div className='footer-btns-wrapper'>
+            <a href='' className='pri-btn'>Browse campaign</a>
+            <a href=''>Get involved</a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
